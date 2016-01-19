14e0edc17bb298966978d86c4584cd35c37204d1
Trevor <thod0127@740s-pl-130.local>
2016-01-19 12:42:05 -0700

    Added header file

---
+++ src/Controller/secondController.h
@@ -0,0 +1,24 @@
+/*
+ * secondController.h
+ *
+ *  Created on: Jan 19, 2016
+ *      Author: thod0127
+ */
+
+#ifndef CONTROLLER_SECONDCONTROLLER_H_
+#define CONTROLLER_SECONDCONTROLLER_H_
+
+class secondController
+{
+private:
+	int classCount;
+public:
+	secondController();
+	void setClassCount(int classCount);
+	int getClassCount();
+	void start();
+};
+
+
+
+#endif /* CONTROLLER_SECONDCONTROLLER_H_ */
